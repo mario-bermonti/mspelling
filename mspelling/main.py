6becb22057a1698f937f5bc84477c017f5f38ee5
Mario Bermonti Perez <mbermonti@Marios-MacBook-Pro-4.local>
2020-10-26 21:38:09 -0400

    kivy basic interface migration
    The ui elements were migrated to kivy. It contains an unpolished version to
    serve as a starting point.

---
+++ mspelling/main.py
@@ -0,0 +1,22 @@
+import kivy
+kivy.require('1.11.1')
+from kivy.app import App
+
+# needs to be imported or kivy won't find them
+# at least the first screen
+from welcomescreen import WelcomeScreen
+from loginscreen import LoginScreen
+from startscreen import StartScreen
+from mspellingroot import MSpellingRoot
+from spellingactivity import SpellingActivityScreen
+from goodbyescreen import GoodbyeScreen
+
+
+class MSpellingApp(App):
+    title = "mDeletreo"
+
+    # def build(self):
+    #     return MSpellingRoot()
+    
+if __name__ == "__main__":
+    MSpellingApp().run()
