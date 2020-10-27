92b2fb126a2ebe74044ec3c0963b57d4d908723c
Mario Bermonti Perez <mbermonti@Marios-MacBook-Pro-4.local>
2020-10-27 19:12:53 -0400

    Add a root widget
    This will help facilitate consolidate functionality for the whole app.

--- mspelling/main.py
+++ mspelling/main.py
@@ -1,6 +1,7 @@
 import kivy
 kivy.require('1.11.1')
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 
 # needs to be imported or kivy won't find them
 # at least the first screen
@@ -10,13 +11,14 @@ from startscreen import StartScreen
 from mspellingroot import MSpellingRoot
 from spellingactivity import SpellingActivityScreen
 from goodbyescreen import GoodbyeScreen
+from mspellingroot import MSpellingRoot
 
 
 class MSpellingApp(App):
     title = "mDeletreo"
 
-    # def build(self):
-    #     return MSpellingRoot()
+    def build(self):
+        return MSpellingRoot()
     
 if __name__ == "__main__":
     MSpellingApp().run()
