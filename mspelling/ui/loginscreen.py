463f7f78b3b5130a4a5c2ed5332ea667cf54dde6
Mario Bermonti Perez <mbermonti@Marios-MacBook-Pro-4.local>
2020-10-29 20:46:16 -0400

    Add the ability to submit the participant's code
    This is part of commit a previous commit, but I didn't add it by mistake.

--- mspelling/ui/loginscreen.py
+++ mspelling/ui/loginscreen.py
@@ -1,4 +1,15 @@
 from kivy.uix.screenmanager import Screen
+from kivy.app import App
 
 class LoginScreen(Screen):
-    pass
+    def submit_participant_code(self, code):
+        """Set the participant's code as an atttibute of the app's root.
+
+        Parameter
+        ---------
+        code (str): participant's code
+        """
+
+        app = App.get_running_app()
+        code = code.strip()
+        app.root.participant_code = code
