ac17456dd16452515d18435cbd91dc43e32a7f1d
Mario Bermonti Perez <mbermonti@Marios-MacBook-Pro-4.local>
2020-11-02 18:08:01 -0400

    Make the app a property of spelling activity
    Avoids continuous calls

--- mspelling/ui/spellingactivity.py
+++ mspelling/ui/spellingactivity.py
@@ -3,17 +3,19 @@ from kivy.app import App
 
 class SpellingActivityScreen(Screen):
     def on_enter(self):
+        self.app = App.get_running_app()
         self.check_if_practice_session()
     def check_if_practice_session(self):
         """Checks whether this is a practice session and sets a flag in
         the app's root to indicate it.
         """
 
-        app = App.get_running_app()
-        code = app.root.participant_code
+        code = self.app.root.participant_code
 
         if len(code) == 0:
-            app.root.is_practice = True
+            self.app.root.is_practice = True
+        else:
+            self.app.root.is_practice = False
         else:
             app.root.is_practice = False
 
