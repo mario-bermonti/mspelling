8100f9881fe225350192db37ef7287dae475db0d
Mario Bermonti Perez <mbermonti@Marios-MacBook-Pro-4.local>
2020-11-30 16:16:03 -0400

    Add test for organizing the trials

--- tests/test_results.py
+++ tests/test_results.py
@@ -24,4 +24,15 @@ class TestResults(object):
 
         assert trial_data_complete.equals(trial_data_returned)
 
+    def test_organize_trial_data_index(self):
+        data = {"word": "gato", "other_data": 5}
+        trial_data = pd.Series(data)
+        trial_data_index_expected = pd.Index(
+            ["participant_id", "word", "response", "other_data"]
+        )
 
+        results = Results(testing=True)
+        trial_data_index_returned = results.organize_trial_data_index(trial_data)
+        print(trial_data_index_returned)
+
+        assert trial_data_index_expected.equals(trial_data_index_returned)
