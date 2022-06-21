46450ec93e7d2aa8b6850c2886044ae2f175c0b2
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2022-06-21 17:24:31 -0400

    Create default text

---
+++ mspelling/lib/screens/components/default_text.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DefaultText extends StatelessWidget {
+  final String text;
+
+  const DefaultText({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(text);
+  }
+}
