446949bc314119c7c702bec7d550a38ab29d37fb
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2022-06-21 15:34:37 -0400

    [python] Remove python version completely
    It was causing probs with the flutter version in VSCode.

--- python_version/mspellingapp/.buildozer/osx/platform/kivy-sdk-packager-master/README.md
+++
@@ -1,4 +0,0 @@
-Kivy SDK Packager
-=================
-
-This repository contains scripts for Kivy SDK generation on Windows, OS X and Linux.
--- python_version/src/mspelling/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Top-level package for mspelling."""
-
-__author__ = """Mario E. Bermonti Pérez"""
-__email__ = "mbermonti1132@gmail.com"
-__version__ = "0.3.0"
