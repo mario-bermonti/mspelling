88279d192182d4dc0e3212ab5b6e9e846e3830c5
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2022-06-22 15:04:49 -0400

    [ui] Define default widget to add space

---
+++ mspelling/lib/screens/components/spacing_holder.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SpacingHolder extends StatelessWidget {
+  const SpacingHolder({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(height: 10);
+  }
+}
