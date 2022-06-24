49746a515f6454eb703bdf7cedd611e881ad154f
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2022-06-24 18:44:45 -0400

    [wid] Require controller for default text field

--- mspelling/lib/components/default_textfield.dart
+++ mspelling/lib/components/default_textfield.dart
@@ -2,15 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:mspelling/styles.dart';
 
 class DefaultTextField extends StatelessWidget {
+  final TextEditingController controller;
+
   const DefaultTextField({
     Key? key,
+    required this.controller,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const TextField(
+    return TextField(
       decoration: textFieldStyle,
       autofocus: true,
+      controller: controller,
     );
   }
 }
