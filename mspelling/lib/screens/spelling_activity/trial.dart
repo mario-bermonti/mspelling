610408d2f3394c38ba5184fcec7727bd924282b7
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2022-06-24 18:49:06 -0400

    [main] Clean code

--- mspelling/lib/screens/spelling_activity/trial.dart
+++ mspelling/lib/screens/spelling_activity/trial.dart
@@ -1,6 +1,3 @@
-import 'dart:async';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:mspelling/constants.dart';
 import 'package:mspelling/components/centeredbox.dart';
