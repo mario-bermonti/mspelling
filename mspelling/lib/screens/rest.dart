a4cfad70cc5e200a0bdb7a6337d7a8125391efff
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2022-06-24 18:56:48 -0400

    [ui] Create rest screen

---
+++ mspelling/lib/screens/rest.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:mspelling/constants.dart';
+import 'package:mspelling/components/default_text.dart';
+import 'package:mspelling/components/spacing_holder.dart';
+import 'package:mspelling/screens/spelling_activity/spelling_screen.dart';
+
+class RestScreen extends StatefulWidget {
+  const RestScreen({Key? key}) : super(key: key);
+
+  @override
+  State<RestScreen> createState() => _RestScreenState();
+}
+
+class _RestScreenState extends State<RestScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(appBarTitle),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const DefaultText(
+              text: 'Descansa',
+            ),
+            const SpacingHolder(),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const SpellingScreen(),
+                  ),
+                );
+              },
+              child: const DefaultText(text: 'Presionar para comenzar'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
