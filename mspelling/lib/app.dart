314d7686a8a8a4b708c5acd1aba5ab30826d5465
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2022-06-24 15:01:40 -0400

    [ui] Add Trial as route and fix other paths

--- mspelling/lib/app.dart
+++ mspelling/lib/app.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:mspelling/screens/end.dart';
 import 'package:mspelling/screens/home.dart';
 import 'package:mspelling/screens/begin_message.dart';
-import 'package:mspelling/screens/spelling.dart';
+import 'package:mspelling/screens/spelling_activity/spelling_screen.dart';
+import 'package:mspelling/screens/spelling_activity/trial.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -18,6 +19,7 @@ class MyApp extends StatelessWidget {
         '/': (context) => const HomeScreen(),
         '/begin': (context) => const BeginScreen(),
         '/spelling': (context) => const SpellingScreen(),
+        '/trial': (context) => const TrialScreen(),
         '/end': (context) => const EndScreen(),
       },
     );
