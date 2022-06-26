2fd1a755a07fdbc19e175fe5d385674139d2aeb1
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2022-06-26 19:18:46 -0400

    [spelling] Present audio stim
    Will require improving code in the future. There is warning for
    the current implementation.

--- mspelling/lib/screens/spelling_activity/trial/trial_stim.dart
+++ mspelling/lib/screens/spelling_activity/trial/trial_stim.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mspelling/constants.dart';
 import 'package:mspelling/components/centeredbox.dart';
 import 'package:mspelling/components/default_text.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class TrialStimScreen extends StatefulWidget {
   final String word;
@@ -9,19 +10,41 @@ class TrialStimScreen extends StatefulWidget {
   const TrialStimScreen({Key? key, required this.word}) : super(key: key);
 
   @override
-  State<TrialStimScreen> createState() => _TrialStimScreenState();
+  // This is not recommended but it was the only way to have access to word
+  // from the state.
+  // Using $widget.word causes errors; this just causes a warning.
+  State<TrialStimScreen> createState() => _TrialStimScreenState(word);
 }
 
 class _TrialStimScreenState extends State<TrialStimScreen> {
-  var controller = TextEditingController();
-  bool allowResponse = true;
+  final audioplayer = AudioPlayer();
+  final String word;
 
-  _TrialStimScreenState() {
+  _TrialStimScreenState(this.word) {
     run();
   }
 
+  // // @override
+  // // void initState() {
+  // //   super.initState();
+  // //   // audioplayer = AudioPlayer();
+  // // }
+
+  // @override
+  // void dispose() {
+  //   audioplayer.dispose();
+  //   super.dispose();
+  // }
+
   void run() {
+    playStim();
     goBack();
+  }
+
+  void playStim() async {
+    AssetSource source = AssetSource('audio/$word.wav');
+    await audioplayer.setSource(source);
+    await audioplayer.play(source);
   }
 
   void goBack() {
