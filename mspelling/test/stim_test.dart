d32f4ca82e8c313404d6f8820a70c3391e2be9cf
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2022-06-28 20:52:29 -0400

    [stim] Organize tests

--- mspelling/test/stim_test.dart
+++ mspelling/test/stim_test.dart
@@ -2,9 +2,18 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mspelling/stim.dart';
 
 void main() {
-  testWidgets('Stim ...', (tester) async {
-    // test('stim ...', () async {
-    final s = await Stimuli.fromFile('assets/words/words.txt');
-    expect(s.stimuli, ['del', 'dos']);
-  });
+  group(
+    'Stim',
+    () {
+      group(
+        'Build from text file',
+        () {
+          testWidgets('Assets folder', (tester) async {
+            final s = await Stimuli.fromFile('assets/words/words.txt');
+            expect(s.stimuli, ['del', 'dos']);
+          });
+        },
+      );
+    },
+  );
 }
