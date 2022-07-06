2c624168601c7dc4633b0cdb6539a83e164f68ad
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2022-07-05 20:47:13 -0400

    Add test for Stim.next

--- mspelling/test/stim_test.dart
+++ mspelling/test/stim_test.dart
@@ -3,6 +3,7 @@ import 'package:mspelling/stim.dart';
 
 void main() async {
   stimFromFileTest();
+  nextStimTest();
 }
 
 stimFromFileTest() async {
@@ -12,6 +13,33 @@ stimFromFileTest() async {
       testWidgets('Assets folder', (tester) async {
         final s = await Stimuli.fromFile('assets/words/words.txt');
         expect(s.stimuli, ['del', 'dos']);
+      });
+    },
+  );
+}
+
+nextStimTest() async {
+  group(
+    'Get next item from stim',
+    () {
+      testWidgets('first item', (tester) async {
+        var stimList = <String>['a', 'b', 'c'];
+        Stimuli stimuli = Stimuli(stimuli: stimList);
+
+        String exp = 'a';
+        String actual = stimuli.next();
+
+        expect(actual, exp);
+      });
+      testWidgets('second item', (tester) async {
+        var stimList = <String>['a', 'b', 'c'];
+        Stimuli stimuli = Stimuli(stimuli: stimList);
+
+        String exp = 'b';
+        stimuli.next();
+        String actual = stimuli.next();
+
+        expect(actual, exp);
       });
     },
   );
