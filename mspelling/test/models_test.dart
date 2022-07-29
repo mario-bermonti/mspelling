d34b5cf5e56b1740f2a9b760696ed85147ed47e6
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2022-07-29 14:28:40 -0400

    [maint] Fix test for models's current date

--- mspelling/test/models_test.dart
+++ mspelling/test/models_test.dart
@@ -1,20 +1,24 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:mspelling/models.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mspelling/models.dart';
 
-// void main() {
-//   testWidgets('models current date', (tester) async {
-//     Session session = Session(
-//       participantId: '01',
-//       timeStart: TimeOfDay.now(),
-//       timeEnd: TimeOfDay.now(),
-//     );
+void main() {
+  testWidgets('models current date, time not included', (tester) async {
+    // obs
+    Session session = Session(
+      participantId: '01',
+      timeStart: TimeOfDay.now(),
+      timeEnd: TimeOfDay.now(),
+    );
 
-//     // DateTime currentDateTime = DateTime.now().toLocal();
-//     // DateTime exp = DateTime(
-//     //     currentDateTime.year, currentDateTime.month, currentDateTime.day);
+    DateTime obs =
+        DateTime(session.date.year, session.date.month, session.date.day);
 
-//     // expect(session.date, exp);
-//     print(session.date);
-//   });
-// }
+    // exp
+    DateTime currentDateTime = DateTime.now().toLocal();
+    DateTime exp = DateTime(
+        currentDateTime.year, currentDateTime.month, currentDateTime.day);
+
+    expect(obs, exp);
+  });
+}
