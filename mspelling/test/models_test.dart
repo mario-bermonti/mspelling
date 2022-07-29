239a6b46ae77d27b2f10c4b286492f906884ac30
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2022-07-29 13:51:11 -0400

    Add test for vanilla session model

---
+++ mspelling/test/models_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mspelling/models.dart';
+
+void main() {
+  testWidgets('models current date', (tester) async {
+    Session session = Session(
+      participantId: '01',
+      timeStart: TimeOfDay.now(),
+      timeEnd: TimeOfDay.now(),
+    );
+
+    DateTime currentDateTime = DateTime.now().toLocal();
+    DateTime exp = DateTime(
+        currentDateTime.year, currentDateTime.month, currentDateTime.day);
+
+    expect(session.date, exp);
+  });
+}
