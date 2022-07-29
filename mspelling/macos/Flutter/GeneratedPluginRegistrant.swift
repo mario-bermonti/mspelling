9d31200730e30d1dba76f8fa72c6e723ac35344e
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2022-07-29 15:43:42 -0400

    [deps] Add drift dep

--- mspelling/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mspelling/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import audioplayers_darwin
 import path_provider_macos
+import sqlite3_flutter_libs
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
 }
