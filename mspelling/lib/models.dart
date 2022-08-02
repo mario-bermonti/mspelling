30ba8a805f1710611bcf00e66a245d40b973cb38
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2022-08-02 16:26:18 -0400

    Add function for getting current platform

--- mspelling/lib/models.dart
+++ mspelling/lib/models.dart
@@ -22,4 +22,18 @@ class Device {
   double height = WidgetsBinding.instance.window.physicalSize.height;
   double width = WidgetsBinding.instance.window.physicalSize.width;
   double aspectRatio = WidgetsBinding.instance.window.physicalSize.aspectRatio;
+
+/// Get the current platform
+/// Options are "web", "android", "fuchsia", "ios", "linux", "macos", "windows"
+String getPlatform({
+  bool webChecker = kIsWeb,
+  String? otherPlatformChecker,
+}) {
+  if (webChecker) {
+    return 'web';
+  }
+
+  /// To have "default value"; [Platform.operatingSystem] is not constant
+  otherPlatformChecker ??= Platform.operatingSystem;
+  return otherPlatformChecker;
 }
