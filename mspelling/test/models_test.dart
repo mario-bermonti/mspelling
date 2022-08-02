cc03dbe8c898c98a93ba52e0ee0d2da5784d08c5
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2022-08-02 16:24:45 -0400

    [maint] Deactivate test that must be migrated to drift

--- mspelling/test/models_test.dart
+++ mspelling/test/models_test.dart
@@ -1,24 +1,28 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mspelling/models.dart';
 
 void main() {
-  testWidgets('models current date, time not included', (tester) async {
-    // obs
-    Session session = Session(
-      participantId: '01',
-      timeStart: TimeOfDay.now(),
-      timeEnd: TimeOfDay.now(),
-    );
+  // testWidgets('models current date, time not included', (tester) async {
+  //   // obs
+  //   Session session = Session(
+  //     participantId: '01',
+  //     timeStart: TimeOfDay.now(),
+  //     timeEnd: TimeOfDay.now(),
+  //   );
 
-    DateTime obs =
-        DateTime(session.date.year, session.date.month, session.date.day);
+  //   DateTime obs = DateTime(session.date.month, session.date.day);
 
-    // exp
-    DateTime currentDateTime = DateTime.now().toLocal();
-    DateTime exp = DateTime(
-        currentDateTime.year, currentDateTime.month, currentDateTime.day);
+  //   // exp
+  //   DateTime currentDateTime = DateTime.now().toLocal();
+  //   DateTime exp = DateTime(
+  //       currentDateTime.year, currentDateTime.month, currentDateTime.day);
 
-    expect(obs, exp);
+  //   expect(obs, exp);
+  // });
+}
+
   });
 }
