0ab5ab2113df10982f27020cca88da4ada968188
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2022-08-14 16:56:33 -0400

    [test] Add test for query insert session

--- mspelling/test/data_test.dart
+++ mspelling/test/data_test.dart
@@ -51,11 +51,67 @@ void main() {
     );
   });
 
+  flutter_test.testWidgets('save session to database', (tester) async {
+    /// exp
+    var exp = SessionsCompanion(
+      sessionNumberParticipant: const Value(1),
+      participantId: const Value('001'),
+      timeStart: Value(DateTime.now()),
+      timeEnd: Value(DateTime.now()),
+    );
+    final int expId = await database.insertSession(exp);
+
+    /// obs
+    final sessionDatabase = await database.getSession(expId);
+    final SessionsCompanion obs = sessionDatabase.toCompanion(true);
+
+    /// checks
+    /// TODO Check if there is a simpler way to compare them
+    /// They are of different type (session vs SessionsCompanion) and
+    /// the former users Value() so compare them.
     flutter_test.expect(obs.id.value, expId);
-    flutter_test.expect(obs.participantId.value, exp.participantId.value);
-    flutter_test.expect(obs.session.value, exp.session.value);
-    flutter_test.expect(obs.stim.value, exp.stim.value);
-    flutter_test.expect(obs.resp.value, exp.resp.value);
+    flutter_test.expect(
+      obs.participantId.value,
+      exp.participantId.value,
+    );
+    flutter_test.expect(
+      obs.sessionNumberParticipant.value,
+      exp.sessionNumberParticipant.value,
+    );
+
+    /// TimeStart
+    /// needs to be converted, to limit precision (different seconds)
+    final DateTime expTimeStartShorten = DateTime(
+      obs.timeStart.value.year,
+      exp.timeStart.value.month,
+      obs.timeStart.value.day,
+    );
+    final DateTime obsTimeStartShorten = DateTime(
+      obs.timeStart.value.year,
+      obs.timeStart.value.month,
+      obs.timeStart.value.day,
+    );
+    flutter_test.expect(
+      obsTimeStartShorten,
+      expTimeStartShorten,
+    );
+
+    /// TimeEnd
+    /// needs to be converted, to limit precision (different seconds)
+    final DateTime expTimeEndShorten = DateTime(
+      obs.timeEnd.value.year,
+      exp.timeEnd.value.month,
+      obs.timeEnd.value.day,
+    );
+    final DateTime obsTimeEndShorten = DateTime(
+      obs.timeEnd.value.year,
+      obs.timeEnd.value.month,
+      obs.timeEnd.value.day,
+    );
+    flutter_test.expect(
+      obsTimeEndShorten,
+      expTimeEndShorten,
+    );
   });
 
   tearDown(() async {
