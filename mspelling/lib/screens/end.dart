4869c1e1540c9b0f91c0cefcc9c19493f70012c2
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2022-12-09 17:14:25 -0400

    [maint] Make end screen stateless

--- mspelling/lib/screens/end.dart
+++ mspelling/lib/screens/end.dart
@@ -2,18 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:mspelling/constants.dart';
 import 'package:mspelling/components/default_text.dart';
 
-class EndScreen extends StatefulWidget {
+class EndScreen extends StatelessWidget {
   /// Communicate the participant the task has ended.
   /// Doesn't allow going back or restarting so the experimenter has control
   /// over the session.
 
   const EndScreen({Key? key}) : super(key: key);
 
-  @override
-  State<EndScreen> createState() => _EndScreenState();
-}
-
-class _EndScreenState extends State<EndScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
