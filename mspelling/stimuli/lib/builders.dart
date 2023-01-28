2052072f696ab88b9c305863e3def36a98598103
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-01-28 19:43:04 -0400

    [stim] Build stim from external dir
    It won't be part of the assets.

--- mspelling/stimuli/lib/builders.dart
+++ mspelling/stimuli/lib/builders.dart
@@ -1,13 +1,16 @@
 /// Functions that help build a Stimuli object from a file listing the stimuli
 
+import 'dart:io';
+
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:stimuli/stim.dart';
 
 /// Build [Stimuli] from a text file specified by
 /// the path filePath.
 Future<Stimuli> createStimFromFile(String filePath) async {
-  String stimString = await rootBundle.loadString(filePath);
-  List<String> stimList = stimString.split('\n');
+  File file = File(filePath);
+  List<String> stimList = await file.readAsLines();
   Stimuli stim = Stimuli(stimuli: stimList);
 
   return stim;
