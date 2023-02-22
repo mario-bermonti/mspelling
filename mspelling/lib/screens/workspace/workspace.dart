a9b2d6de1def0cbd23ef81c23982ebb688e031aa
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-02-22 16:18:26 -0400

    Create and add workspace screen

---
+++ mspelling/lib/screens/workspace/workspace.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:mspelling/constants.dart';
+import 'package:mspelling/components/centeredbox.dart';
+import 'package:mspelling/components/default_text.dart';
+import 'package:mspelling/screens/login.dart';
+import 'package:mspelling/screens/workspace/utils.dart';
+
+class SetWorkspaceScreen extends StatefulWidget {
+  const SetWorkspaceScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SetWorkspaceScreen> createState() => _SetWorkspaceScreenState();
+}
+
+class _SetWorkspaceScreenState extends State<SetWorkspaceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+        onWillPop: () async {
+          return false;
+        },
+        child: Scaffold(
+          appBar: AppBar(title: const Text(appBarTitle)),
+          body: _buildUI(context),
+        ));
+  }
+
+  CenteredBox _buildUI(BuildContext context) {
+    return CenteredBox(
+      column: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          ElevatedButton(
+            onPressed: () {
+              setWorkspaceByUser().then((value) => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const LoginScreen()),
+                  ));
+            },
+            child: const DefaultText(text: 'Selecciona área de trabajo'),
+          ),
+        ],
+      ),
+    );
+  }
+}
