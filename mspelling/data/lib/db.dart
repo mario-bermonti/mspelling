eba613713c7b4df44b01e45806ef281d9ef3e9fe
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-03-05 20:14:24 -0400

    [data] [maint] Clean code

--- mspelling/data/lib/db.dart
+++ mspelling/data/lib/db.dart
@@ -5,8 +5,6 @@ import 'dart:io';
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:data/models.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:path/path.dart' as p;
 import 'package:permission_handler/permission_handler.dart';
 
 part 'db.g.dart';
