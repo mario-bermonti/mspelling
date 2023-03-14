f9a02c8c1ce80855056a3ffb7db7da58f46fda95
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-03-14 16:26:26 -0400

    [docs] Document code in utils

--- mspelling/lib/screens/workspace/utils.dart
+++ mspelling/lib/screens/workspace/utils.dart
@@ -1,6 +1,7 @@
 import 'package:file_picker/file_picker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+/// Let the user set the workspace
 Future<void> setWorkspaceByUser() async {
   String? selectedDirectory = await FilePicker.platform.getDirectoryPath();
 
@@ -14,6 +15,7 @@ Future<void> setWorkspaceByUser() async {
   }
 }
 
+/// Get the workspace that will be used for accessing stim and saving data
 Future<String?> getWorkspace() async {
   SharedPreferences settings = await SharedPreferences.getInstance();
   String? workSpace = settings.getString('workspace');
