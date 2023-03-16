69ca3d5a9e2f9bbe9344c9a111367758615755f0
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-03-16 17:50:13 -0400

    Add home button to action buttons

--- mspelling/lib/components/default_appbar.dart
+++ mspelling/lib/components/default_appbar.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:mspelling/constants.dart';
 import 'package:mspelling/screens/workspace/utils.dart';
+import 'package:mspelling/setup_manager.dart';
 
-createAppBar({bool showWorkspaceButton = false}) {
+createAppBar({required BuildContext context, bool showActionButtons = false}) {
   return AppBar(
       title: const Text(appBarTitle),
       automaticallyImplyLeading: false,
-      actions: showWorkspaceButton ? workspaceButton : null);
+      actions:
+          showActionButtons ? createActionButtons(context: context) : null);
 }
 
-const List<Widget> workspaceButton = <Widget>[
-  IconButton(
-    icon: Icon(Icons.settings),
-    tooltip: 'Directorio de trabajo',
-    onPressed: setWorkspaceByUser,
-  ),
-];
+List<Widget> createActionButtons({required BuildContext context}) {
+  List<Widget> actionButtons = <Widget>[
+    IconButton(
+      icon: const Icon(Icons.home),
+      tooltip: 'Inicio',
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const SetupManager(),
+          ),
+        );
+      },
+    ),
+    const IconButton(
+      icon: Icon(Icons.settings),
+      tooltip: 'Directorio de trabajo',
+      onPressed: setWorkspaceByUser,
+    ),
+  ];
+  return actionButtons;
+}
