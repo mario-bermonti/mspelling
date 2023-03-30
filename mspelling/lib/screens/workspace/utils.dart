d26ac97caf5c6cad83421f495ac4cd215ed367fb
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-03-30 16:17:49 -0400

    Request storage permission for stim

--- mspelling/lib/screens/workspace/utils.dart
+++ mspelling/lib/screens/workspace/utils.dart
@@ -2,12 +2,14 @@ import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 /// Let the user set the workspace and validate it
 Future<void> setWorkspaceByUser() async {
   String? selectedDirectory = await FilePicker.platform.getDirectoryPath();
 
   if (selectedDirectory != null) {
+    await getPermissionIfNecessary(selectedDirectory);
     await validate(selectedDirectory);
     SharedPreferences settings = await SharedPreferences.getInstance();
     settings.setString('workspace', selectedDirectory);
@@ -28,5 +30,18 @@ Future<void> validate(String workspace) async {
 
   if (result == false) {
     throw Exception('The stim file was not found in workspace/stim');
+  }
+}
+
+/// Get permission to use the path provided if necessary
+/// Throws error if permission is needed and not granted
+/// [path] String path where to access the workspace
+Future<void> getPermissionIfNecessary(String path) async {
+  if (Platform.isAndroid) {
+    bool granted = await Permission.storage.request().isGranted;
+    if (!granted) {
+      throw Exception(
+          'Permission for accessing the specified workspace was requested but not granted');
+    }
   }
 }
