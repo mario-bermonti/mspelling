05f869c51d4642e1b90642da3ccf91d1cd4a2504
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-04-09 11:53:10 -0400

    [stim] Throw error if stim.txt doesn't exist

--- mspelling/stimuli/lib/builders.dart
+++ mspelling/stimuli/lib/builders.dart
@@ -6,7 +6,12 @@ import 'package:stimuli/stim.dart';
 /// Build [Stimuli] from a text file specified by
 /// the path filePath.
 Future<Stimuli> createStimFromFile(String filePath) async {
+  /// TODO fail if file doesn't exist
+  /// TODO refactor into 2 methods?
   File file = File(filePath);
+  if (await file.exists() == false) {
+    throw Exception('stim.txt file not found in $filePath');
+  }
   List<String> stimList = await file.readAsLines();
   Stimuli stim = Stimuli(stimuli: stimList);
 
