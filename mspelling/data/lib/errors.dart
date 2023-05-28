6f0c8147de7ba323afabfb8a930e53bdd769094b
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-05-28 17:53:21 -0400

    [data] Define custom permission exception

---
+++ mspelling/data/lib/errors.dart
@@ -0,0 +1,7 @@
+class ErrorPermissionNotGranted implements Exception {
+  String message =
+      'Could not read or write data to file due to a permission error';
+
+  @override
+  String toString() => message;
+}
