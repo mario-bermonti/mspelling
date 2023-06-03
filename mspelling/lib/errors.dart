9392498bc66257cbadc5b777e69caf3f10db0054
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-06-03 16:51:00 -0400

    [mspelling][maint] Clean dead code

--- mspelling/lib/errors.dart
+++ mspelling/lib/errors.dart
@@ -1,18 +1,3 @@
-// Define errors: exception and name
-
-Map<String, dynamic> errorWorkspaceAccess = <String, dynamic>{
-  'userMessage': 'El área de trabajo no fue encontrado',
-  'exceptionMessage': 'Could not access the workspace. It is probably not set',
-};
-Map<String, dynamic> errorStimFileNotFound = <String, dynamic>{
-  'userMessage': 'Los estímulos no fueron encontrados en el área de trabajo',
-  'exceptionMessage': 'Stim file not found in workspace',
-};
-Map<String, dynamic> errorPermissionNotGranted = <String, dynamic>{
-  'userMessage': 'No tienes acceso para leer o escribir datos',
-  'exceptionMessage': 'Could not read or write data to file due to permission',
-};
-
 abstract class MSpellingExeption implements Exception {
   String userMessage();
 }
@@ -61,9 +46,3 @@ class PermissionNotGrantedException implements MSpellingExeption {
   @override
   String toString() => exceptionMessage;
 }
-// define function for throwing errors
-
-// define exceptions
-// workspace not found
-// Data not found - stim file
-// permission not granted
