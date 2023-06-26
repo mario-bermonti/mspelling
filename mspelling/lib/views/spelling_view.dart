9a3602f103e07ab4036f7b916119736d76d926ad
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-06-26 12:59:31 -0400

    Add basic view for spelling activity

---
+++ mspelling/lib/views/spelling_view.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:mspelling/controllers/status.dart';
+import 'package:mspelling/errors.dart';
+import 'package:mspelling/screens/end.dart';
+import 'package:mspelling/screens/errors.dart';
+import 'package:mspelling/screens/rest.dart';
+import 'package:mspelling/screens/spelling_activity/trial/trial_response_screen.dart';
+import 'package:mspelling/screens/spelling_activity/trial/trial_stim_screen.dart';
+
+// Controls the task's sequences
+// Has no ui representation itself
+class SpellingView extends StatefulWidget {
+  final String participantId;
+
+  const SpellingView(this.participantId, {Key? key}) : super(key: key);
+
+  @override
+  State<SpellingView> createState() => _SpellingViewState();
+}
+
+class _SpellingViewState extends State<SpellingView> {
+  /// Flag to indicate whether the ui can be displayed
+  late Future<bool> setupDone;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: setupDone,
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError) {
+              /// TODO it's necessary to cast as mspelling error?
+              return ErrorScreen(message: snapshot.error as MSpellingExeption);
+            } else {
+              switch (status) {
+                case Status.stim:
+                  return TrialStimScreen(
+                    workspace: _workspace!,
+                    stim: _stimuli.currentStim,
+                  );
+                case Status.response:
+                  return const TrialResponseScreen();
+                case Status.rest:
+                  return RestScreen(widget.participantId);
+                case Status.completed:
+                  return const EndScreen();
+              }
+            }
+          } else {
+            return WillPopScope(
+              onWillPop: () async {
+                return false;
+              },
+              child: const Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+        });
+  }
+}
