1d05d866cf7523410e84f2d4e8c0b80c94d2c11a
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-07-12 19:20:31 -0400

    [maint] Don't check permission workspace

--- mspelling/lib/controllers/workspace_controller.dart
+++ mspelling/lib/controllers/workspace_controller.dart
@@ -1,11 +1,7 @@
-import 'dart:io';
 import 'package:file_picker/file_picker.dart';
 import 'package:get/get.dart';
-import 'package:get/get_state_manager/get_state_manager.dart';
-import 'package:mspelling/errors.dart';
 import 'package:mspelling/screens/login.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 class WorkspaceController extends GetxController {
   /// Let the user set the workspace and validate it
@@ -13,7 +9,6 @@ class WorkspaceController extends GetxController {
     String? selectedDirectory = await FilePicker.platform.getDirectoryPath();
 
     if (selectedDirectory != null) {
-      await getPermissionIfNecessary(path: selectedDirectory);
       SharedPreferences settings = await SharedPreferences.getInstance();
       await settings.setString('workspace', selectedDirectory);
     }
@@ -24,18 +19,6 @@ class WorkspaceController extends GetxController {
     SharedPreferences settings = await SharedPreferences.getInstance();
     String? workSpace = settings.getString('workspace');
     return workSpace;
-  }
-
-  /// Get permission to use the path provided if necessary
-  /// Throws error if permission is needed and not granted
-  /// [path] String path where to access the workspace
-  Future<void> getPermissionIfNecessary({required String path}) async {
-    if (Platform.isAndroid) {
-      bool granted = await Permission.storage.request().isGranted;
-      if (!granted) {
-        throw PermissionNotGrantedException();
-      }
-    }
   }
 
   void nextScreen() {
