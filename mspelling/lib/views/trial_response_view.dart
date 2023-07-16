dfc7f386a34e5a3f0d1b352aa0b42cfce798557d
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-07-16 17:03:37 -0400

    Add trial response view

---
+++ mspelling/lib/views/trial_response_view.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mspelling/components/centeredbox.dart';
+import 'package:mspelling/components/default_appbar.dart';
+import 'package:mspelling/components/default_text.dart';
+import 'package:mspelling/components/default_textfield.dart';
+import 'package:mspelling/components/spacing_holder.dart';
+import 'package:mspelling/controllers/trial_response_controller.dart';
+
+class TrialResponseView extends StatelessWidget {
+  /// Screen for collecting response from participant
+
+  final TrialResponseController trialResponseController =
+      Get.put(TrialResponseController());
+
+  TrialResponseView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        return false;
+      },
+      child: Scaffold(
+        appBar: createAppBar(context: context),
+        body: CenteredBox(
+          column: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const DefaultText(
+                text: 'Escribe la palabra:',
+              ),
+              const BetweenWidgetsSpace(),
+              DefaultTextField(
+                  controller: trialResponseController.textController),
+              const BetweenWidgetsSpace(),
+              ElevatedButton(
+                onPressed: () {
+                  trialResponseController.submit();
+                  trialResponseController.toNextScreen();
+                },
+                child: const DefaultText(text: 'Seguir'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
