940b665ef9a144d025166107c4107ec730cfecdc
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-07-16 15:28:09 -0400

    Use stim controller in trial stim view

--- mspelling/lib/views/trial_stim_view.dart
+++ mspelling/lib/views/trial_stim_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mspelling/components/default_appbar.dart';
 import 'package:mspelling/components/centeredbox.dart';
-import 'package:mspelling/controllers/trial_stim_controller.dart';
+import 'package:mspelling/controllers/stim_controller.dart';
 
 class TrialStimView extends StatelessWidget {
   /// In this screen we present the stims to participants
@@ -11,13 +11,11 @@ class TrialStimView extends StatelessWidget {
     Key? key,
   }) : super(key: key);
 
-  final TrialStimController trialstimController =
-      Get.put(TrialStimController());
+  final StimController stimController = Get.find();
 
   @override
   Widget build(BuildContext context) {
-    // trialstimController.prepareStim();
-    trialstimController.presentStim();
+    stimController.presentStim();
     return WillPopScope(
         onWillPop: () async {
           return false;
