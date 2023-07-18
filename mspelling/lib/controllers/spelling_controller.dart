d20cf1f5c2f88fcddd5a06c372db2bb007e7f323
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-07-17 21:03:42 -0400

    [maint] Make all spelling controller attrs private

--- mspelling/lib/controllers/spelling_controller.dart
+++ mspelling/lib/controllers/spelling_controller.dart
@@ -13,25 +13,25 @@ import 'package:mspelling/views/trial_stim_view.dart';
 // Controls the task's sequences
 class SpellingController extends GetxController {
   // Stimuli used in the task
-  late final StimController stimuli;
+  late final StimController _stimuli;
 
   /// Global task start time
   final DateTime _timeStart = DateTime.now();
-  late final DataBase database;
+  late final DataBase _database;
 
   /// Session  number for current participant
-  late final int sessionNumber;
+  late final int _sessionNumber;
 
-  Step status = Step.stim;
+  Step _status = Step.stim;
 
-  final LoginController loginController = Get.find();
-  late final String participantId;
+  final LoginController _loginController = Get.find();
+  late final String _participantId;
 
-  final SetupController setupController = Get.find();
+  final SetupController _setupController = Get.find();
 
   @override
   onInit() async {
-    await setup();
+    await _setup();
     super.onInit();
   }
 
@@ -43,91 +43,91 @@ class SpellingController extends GetxController {
   /// Helper method to get the session number for the current participant
   /// from the db
   Future<void> _getSessionNumberParticipant() async {
-    sessionNumber =
-        await database.getCurrentParticipantSessionNumber(participantId);
+    _sessionNumber =
+        await _database.getCurrentParticipantSessionNumber(_participantId);
   }
 
   /// Controls the sequence of the task, including presenting trials, rests,
   /// ITI, ending the session.
   /// [context] BuildContext: Needed to navigate
   void addTrialData({required String result}) {
-    database.addTrialData(
-      participantId: participantId,
-      stim: stimuli.stim.currentStim,
+    _database.addTrialData(
+      participantId: _participantId,
+      stim: _stimuli.stim.currentStim,
       resp: result,
-      sessionNumber: sessionNumber,
+      sessionNumber: _sessionNumber,
     );
   }
 
   // TODO improve name of conditions checks?
   /// TODO can presenting stim next be improved? Current implementation seems
   /// weird
-  void updateStatus() {
-    if (responseStatusFollows()) {
-      status = Step.response;
-    } else if (completedStatusFollows()) {
-      status = Step.completed;
-    } else if (stimStatusFollows()) {
-      status = Step.stim;
-    } else if (restStatusFollows()) {
-      status = Step.rest;
+  void _updateStatus() {
+    if (_responseStatusFollows()) {
+      _status = Step.response;
+    } else if (_completedStatusFollows()) {
+      _status = Step.completed;
+    } else if (_stimStatusFollows()) {
+      _status = Step.stim;
+    } else if (_restStatusFollows()) {
+      _status = Step.rest;
     } else {
-      status = Step.stim;
+      _status = Step.stim;
     }
   }
 
-  bool responseStatusFollows() => status == Step.stim;
-  bool stimStatusFollows() => status == Step.rest;
-  bool restStatusFollows() =>
-      stimuli.stim.stimCountUsed != 0 && stimuli.stim.stimCountUsed % 5 == 0;
-  bool completedStatusFollows() => stimuli.stim.stimCountRemaining == 0;
+  bool _responseStatusFollows() => _status == Step.stim;
+  bool _stimStatusFollows() => _status == Step.rest;
+  bool _restStatusFollows() =>
+      _stimuli.stim.stimCountUsed != 0 && _stimuli.stim.stimCountUsed % 5 == 0;
+  bool _completedStatusFollows() => _stimuli.stim.stimCountRemaining == 0;
 
   void _endSession() {
     /// Global session end time
     final DateTime timeEnd = DateTime.now();
 
-    database.addSessionData(
-        sessionNumber: sessionNumber,
-        participantId: participantId,
+    _database.addSessionData(
+        sessionNumber: _sessionNumber,
+        participantId: _participantId,
         timeStart: _timeStart,
         timeEnd: timeEnd);
-    database.addDeviceData(
-      participantId: participantId,
-      sessionNumber: sessionNumber,
+    _database.addDeviceData(
+      participantId: _participantId,
+      sessionNumber: _sessionNumber,
     );
-    database.saveData();
+    _database.saveData();
   }
 
   /// Get workspace, prepare stim, prepare db, and start spelling activity
   /// Throws error if the workspace is null (hasn't been set)
-  Future<void> setup() async {
-    participantId = loginController.participantID;
-    String? workspace = await setupController.workspace;
+  Future<void> _setup() async {
+    _participantId = _loginController.participantID;
+    String? workspace = await _setupController.workspace;
     if (workspace == null) {
       throw WorkspaceAccessException();
 
       /// TODO check if this else is necessary
     } else {
-      stimuli = Get.put(StimController(stimPath: workspace));
-      await stimuli.prepareStim();
-      database = await getDB(path: '$workspace/mspelling_data.sqlite3');
+      _stimuli = Get.put(StimController(stimPath: workspace));
+      await _stimuli.prepareStim();
+      _database = await getDB(path: '$workspace/mspelling_data.sqlite3');
     }
     await _getSessionNumberParticipant();
   }
 
   void run() {
-    switch (status) {
+    switch (_status) {
       case Step.stim:
         Get.to(() => TrialStimView());
-        updateStatus();
+        _updateStatus();
         break;
       case Step.response:
         Get.to(() => TrialResponseView());
-        updateStatus();
+        _updateStatus();
         break;
       case Step.rest:
         Get.to(() => RestView());
-        updateStatus();
+        _updateStatus();
         break;
       case Step.completed:
         _endSession();
