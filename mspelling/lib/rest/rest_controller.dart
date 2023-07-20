dfa675b4712196879b7f7c960404a9b99424711b
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-07-20 15:41:56 -0400

    [doc] Improve docstrings for rest controller

--- mspelling/lib/rest/rest_controller.dart
+++ mspelling/lib/rest/rest_controller.dart
@@ -4,6 +4,7 @@ import 'package:mspelling/activity/spelling_controller.dart';
 class RestController extends GetxController {
   SpellingController spellingController = Get.find();
 
+  /// Goes to next screen after 500ms ISI
   void toNextScreen() {
     Future.delayed(const Duration(milliseconds: 500), () {
       spellingController.run();
