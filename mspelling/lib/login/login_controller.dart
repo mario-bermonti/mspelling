ac0a85459cc0427f0478d8e792b79ba649fe5b36
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-07-20 15:40:51 -0400

    [doc] Improve docstrings for login controller

--- mspelling/lib/login/login_controller.dart
+++ mspelling/lib/login/login_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+/// Manager that holds the participant ID
 class LoginController extends GetxController {
   final TextEditingController textController = TextEditingController();
   late final String participantID;
