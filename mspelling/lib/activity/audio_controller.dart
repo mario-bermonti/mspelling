fd4327dd37463586b3f9de2084a8148b89707d33
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2023-07-20 13:44:53 -0400

    [doc] Improve documentation fo audio controller

--- mspelling/lib/activity/audio_controller.dart
+++ mspelling/lib/activity/audio_controller.dart
@@ -1,22 +1,25 @@
+/// Provides an audio player manager
+
 import 'dart:io';
 import 'package:get/get.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:mspelling/errors/errors.dart';
 import 'package:mspelling/errors/error_view.dart';
 
+/// Manager for the audio player
+/// Is is used to present the stim to participants
 class AudioController extends GetxController {
   final AudioPlayer _audioplayer = AudioPlayer();
 
-  /// Present from file audio
+  /// Play audio from [path]
   Future<void> playAudio(String path) async {
     await _audioplayer.play(DeviceFileSource(path));
   }
 
-  /// Validate the audio file exists
+  /// Validate the audio file specified in [path] exists
   Future<void> validateAudioStimFile(String path) async {
     File file = File(path);
     if (await file.exists() == false) {
-      /// Should throw audio or other general exception
       Get.to(
         () => ErrorView(
           message:
